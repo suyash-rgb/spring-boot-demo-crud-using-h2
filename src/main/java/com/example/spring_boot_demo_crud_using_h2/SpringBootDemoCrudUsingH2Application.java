5699be6672fb42c11d5ccf7ead4051172faa4190
Suyash Baoney <suyashbaoney58@gmail.com>
2025-07-02 22:54:49 +0530

    Initial commit

---
+++ src/main/java/com/example/spring_boot_demo_crud_using_h2/SpringBootDemoCrudUsingH2Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_demo_crud_using_h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoCrudUsingH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoCrudUsingH2Application.class, args);
+	}
+
+}
